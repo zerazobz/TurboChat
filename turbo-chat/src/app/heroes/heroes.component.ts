c5b81343455af8319000d20d0b70beb8e6767368
Erick Asto Oblitas <erickastooblitas>
2019-10-09 01:25:45 -0500

    Added Detail section

--- turbo-chat/src/app/heroes/heroes.component.ts
+++ turbo-chat/src/app/heroes/heroes.component.ts
@@ -8,12 +8,16 @@ import { HEROES } from '../mock-heroes';
 	styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+	selectedHero: Hero;
 	hero: Hero = {
 		id: 1,
 		name: "Windstorm 龙。"
 	}
 	heroes = HEROES
-	
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
+
 	constructor() { }
 
   	ngOnInit() {
