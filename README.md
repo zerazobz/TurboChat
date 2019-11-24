ee94679df3293894752d00de485a09160aecbcfa
Erick Asto Oblitas <erickastooblitas>
2019-11-24 00:34:20 -0500

    basic service integration

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ At this moment I'm implemented the following items from the [main Angular Tutori
 1. The hero editor
 2. Displaying a list
 3. Master/Detail Components
+4. Services
+	Components shouldn't fetch or save data directly and they shouldn't knowingly present fake data. They should focus on presenting data and delegate data access to a service.
--- turbo-chat/src/app/heroes/heroes.component.ts
+++ turbo-chat/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
 	selector: 'app-heroes',
@@ -13,13 +13,17 @@ export class HeroesComponent implements OnInit {
 		id: 1,
 		name: "Windstorm 龙。"
 	}
-	heroes = HEROES
+	heroes: Hero[];
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
 
-	constructor() { }
+	constructor(private heroService: HeroService) { }
 
   	ngOnInit() {
+        this.getHeroes();
+  	}
+  	getHeroes() {
+        this.heroes = this.heroService.getHeroes();
   	}
 }
